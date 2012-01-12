ae65d3cd4df83e1c2505896135dd002127792530
Dennis Iversen <dennis.iversen@gmail.com>
2012-01-12 18:52:12 +0100

    refactor

--- blocks/debug.php
+++ blocks/debug.php
@@ -18,11 +18,11 @@ function block_debug(){
     //$info = moduleLoader::getModuleIniSettings('google_contact');
     print "<a name=\"debug_block\">Debug block</a><br />";
     print "<ul>\n";
-    print "<li>" . create_link($_SERVER['REQUEST_URI'] . "#benchmark", " Benchmark") . "<br />";
-    print "<li>" . create_link($_SERVER['REQUEST_URI'] . "#main_settings", " Main Settings") . "<br />";
-    print "<li>" . create_link($_SERVER['REQUEST_URI'] . "#module_settings", " Module Settings") . "<br />";
-    print "<li>" . create_link($_SERVER['REQUEST_URI'] . "#sql", " SQL") . "<br />";
-    print "<li>" . create_link($_SERVER['REQUEST_URI'] . "#server", " Server Info") . "<br />";
+    print "<li>" . html::createLink($_SERVER['REQUEST_URI'] . "#benchmark", " Benchmark") . "<br />";
+    print "<li>" . html::createLink($_SERVER['REQUEST_URI'] . "#main_settings", " Main Settings") . "<br />";
+    print "<li>" . html::createLink($_SERVER['REQUEST_URI'] . "#module_settings", " Module Settings") . "<br />";
+    print "<li>" . html::createLink($_SERVER['REQUEST_URI'] . "#sql", " SQL") . "<br />";
+    print "<li>" . html::createLink($_SERVER['REQUEST_URI'] . "#server", " Server Info") . "<br />";
     print "</ul>\n";
     //print $info['info'];
     //get_module_ini('info');
