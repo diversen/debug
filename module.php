778b8752a55d1843ad5d437f4471f1f7ad4d4b45
Dennis Iversen <dennis.iversen@gmail.com>
2016-03-12 14:13:26 +0100

    refactor. Hide config.ini secrets. use uikit table. docs

--- module.php
+++ module.php
@@ -3,30 +3,46 @@
 namespace modules\debug;
 
 use diversen\conf;
-use diversen\db;
-use diversen\template;
-use diversen\template\assets;
+use diversen\db\admin;
+use diversen\db\connect;
+use diversen\db\q;
 use diversen\file\string as fileStr;
+use diversen\template;
+use diversen\profile;
 
 /**
- * Primitive class for adding debug info
- * @package    debug
- */
-
-//assets::setInlineCss(conf::pathModules() . '/debug/table.css');
-
-/**
- * Class adding primitive debug info
- * @package    debug
+ * Debug module class
  */
 class module {
+    
+    /**
+     * Checks if connection is MySQL
+     * @return boolean $res
+     */
+    public function isMySQL () {
+        $info = admin::getDbInfo();
+        if ($info['scheme'] == 'mysql' OR $info['scheme'] == 'mysqli') {
+            return true;
+        }
+        return false;
+    }
 
     /**
+     * Echo HTML debug tables
      * @param int  $level the runlevel
-     * @return  
+     * @return void
      */
     public function runLevel($level) {
 
+        // Only on level 1
+        if ($level == 1) {
+            if ($this->isMySQL()) {
+                q::query("set profiling=1;")->exec();
+            }
+            return;
+        }
+
+        // Level 7
         $debug = conf::getMainIni('debug');
         if (!$debug) {
             return '';
@@ -34,14 +50,24 @@ class module {
 
         ob_start();
         
+        echo "<br />";
         echo '<a name="main-settings" class="main-settings">All ini Settings</a>';
-        self::echoArrayDiv(conf::$vars['coscms_main'], 'debug-main');
+        
+        $p = new profile();
+        $config = $p->iniArrayPrepare(conf::$vars['coscms_main']);
+        self::echoArrayDiv($config, 'debug-main');
                 
         echo '<a name="modules" class="modules">Only Module Settings</a>';
         self::echoArrayDiv(conf::$vars['coscms_main']['module'], 'debug-modules');
 
         echo '<a name="sql" class="sql">SQL</a>';
-        self::echoArrayDiv(db::getDebug(), 'debug-sql');
+        self::echoArrayDiv(connect::getDebug(), 'debug-sql');
+        
+        if ($this->isMySQL()) {
+            $rows = q::query('SHOW PROFILES;')->fetch();            
+            echo '<a name="profiler" class="profiler">MySQL profiler</a>';
+            self::echoArrayDiv($rows, 'debug-profiler');
+        }
       
         echo '<a name="server" class="server">Server Info</a>';
         self::echoArrayDiv($_SERVER, 'debug-server');
@@ -75,6 +101,10 @@ $( ".modules" ).click(function() {
 
 $( ".sql" ).click(function() {
   $( ".debug-sql" ).toggle();
+});
+
+$( ".profiler" ).click(function() {
+  $( ".debug-profiler" ).toggle();
 });
 
 $( ".server" ).click(function() {
@@ -113,14 +143,13 @@ $( ".log" ).click(function() {
     public static function getOffset($level) {
         $offset = "";
         for ($i = 1; $i < $level; $i++) {
-            $offset = $offset . "<td id='tabledebug'></td>";
+            $offset = $offset . "<td></td>";
         }
         return $offset;
     }
 
     /**
-     * function for echoing an array in a table
-     *
+     * Method that echos an array as a HTML table
      * @param   array   the array to be shown in a table
      * @param   int     number of levels
      * @param   int     sub
@@ -131,22 +160,22 @@ $( ".log" ).click(function() {
             foreach ($array as $key_val => $value) {
                 $offset = "";
                 if (is_array($value) == 1) {   // array is multidimensional
-                    echo "<tr id='tabledebug'>";
+                    echo "<tr>";
                     $offset = self::getOffset($level);
-                    echo $offset . "<td id='tabledebug'>" . $key_val . "</td>";
+                    echo $offset . "<td>" . $key_val . "</td>";
                     self::displayArray($value, $level + 1, 1);
                 } else {
                     if ($sub != 1) {
-                        echo "<tr id='tabledebug'>";
+                        echo "<tr>";
                         $offset = self::getOffset($level);
                     }
                     $sub = 0;
                     
                     echo $offset . 
-                            "<td id='tabledebug'>" . 
+                            "<td>" . 
                             $key_val .
                             "</td>" . 
-                            "<td id='tabledebug'>" . 
+                            "<td>" . 
                             $value . 
                             "</td>";
                     
@@ -164,12 +193,12 @@ $( ".log" ).click(function() {
      * @param   array   the array to create a html table from
      */
     public static function echoArrayDiv($array, $class) {
+
         echo '<div class="debug-table ' . $class . '" style="display: none">';
-        echo "<table>\n";
+        echo '<table class="uk-table">';
         self::displayArray($array, 1, 0);
         echo "</table>\n";
         echo "</div>\n";
         echo "<br />";
     }
-
 }
